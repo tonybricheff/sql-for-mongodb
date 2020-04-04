d0e885574351ff4c94abdb50f0275d519fb0d88b
tonybricheff <tony.bricheff@yandex.ru>
2020-04-04 21:21:51 +0300

    predicates support added

--- src/ru/ifmo/rain/brichev/Main.java
+++ src/ru/ifmo/rain/brichev/Main.java
@@ -1,8 +1,13 @@
 package ru.ifmo.rain.brichev;
 
+import com.sun.jdi.request.InvalidRequestStateException;
+
+import javax.naming.OperationNotSupportedException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.prefs.InvalidPreferencesFormatException;
 
 
 public class Main {
@@ -12,12 +17,19 @@ public class Main {
         private int fromPosition;
         private int limitPosition;
         private int offsetPosition;
+        private int wherePosition;
 
-        private void parseMainOperators(String sqlCommand) {
+        private void parseOperators(String sqlCommand) throws SQLException {
             selectPosition = sqlCommand.indexOf("SELECT");
             fromPosition = sqlCommand.indexOf("FROM");
             limitPosition = sqlCommand.indexOf("LIMIT");
             offsetPosition = sqlCommand.indexOf("OFFSET");
+            wherePosition = sqlCommand.indexOf("WHERE");
+
+            if (selectPosition == -1 || fromPosition == -1) {
+                System.out.println("Invalid command format");
+                throw new SQLException();
+            }
         }
 
         private List<String> getColumns(String sqlCommand) {
@@ -28,10 +40,34 @@ public class Main {
             return columns;
         }
 
+        private List<String> getPredicates(String sqlCommand) {
+            List<String> predicates = new ArrayList<>();
+            StringTokenizer stringTokenizer = new StringTokenizer(sqlCommand.substring(wherePosition + 5), " AND");
+            while (stringTokenizer.hasMoreTokens())
+                predicates.add(stringTokenizer.nextToken());
+            return predicates;
+        }
+
+        private String validatePredicate(String predicate, String value) throws UnsupportedOperationException {
+            switch (predicate) {
+                case "=":
+                    return value;
+                case ">":
+                    return "{$gt: " + value + "}";
+                case "<":
+                    return "{$lt: " + value + "}";
+                case "<>":
+                    return "{$ne: " + value + "}";
+                default:
+                    throw new UnsupportedOperationException();
+            }
+        }
+
         private String parseToken(String sqlCommand, int position) {
             StringTokenizer stringTokenizer = new StringTokenizer(sqlCommand.substring(position));
             return stringTokenizer.nextToken();
         }
+
 
         private void addColumns(StringBuilder mongoCommand, List<String> columns) {
             mongoCommand.append(", {");
@@ -41,14 +77,31 @@ public class Main {
             mongoCommand.append("})");
         }
 
+        private void addPredicates(StringBuilder mongoCommand, List<String> predicates) {
+            for (int i = 0; i < predicates.size() - 2; i += 3) {
+                mongoCommand.append(predicates.get(i)).append(": ").append(validatePredicate(predicates.get(i + 1),
+                        predicates.get(i + 2))).append(", ");
+            }
 
-        private String createMongoFind(String sqlCommand, String tableName, List<String> columns) {
+            mongoCommand.deleteCharAt(mongoCommand.length() - 2).deleteCharAt(mongoCommand.length() - 1);
+            mongoCommand.append("}");
+        }
+
+
+        private String createMongoFind(String sqlCommand) {
             StringBuilder mongoCommand = new StringBuilder();
-            mongoCommand.append("db.").append(tableName).append(".find({}");
+            mongoCommand.append("db.").append(parseToken(sqlCommand, fromPosition + 5)).append(".find({");
+            if (wherePosition != -1) {
+                addPredicates(mongoCommand, getPredicates(sqlCommand));
+            } else {
+                mongoCommand.append("}");
+            }
+            List<String> columns = getColumns(sqlCommand);
             if (!columns.get(0).equals("*")) {
                 addColumns(mongoCommand, columns);
-            } else
+            } else {
                 mongoCommand.append(")");
+            }
             if (offsetPosition != -1)
                 mongoCommand.append(".skip(").append(parseToken(sqlCommand, offsetPosition + 6)).append(")");
             if (limitPosition != -1)
@@ -60,10 +113,14 @@ public class Main {
 
 
         public void convert(String sqlCommand) {
-            parseMainOperators(sqlCommand);
-            System.out.println(createMongoFind(sqlCommand, parseToken(sqlCommand, fromPosition + 5), getColumns(sqlCommand)));
+            try {
+                parseOperators(sqlCommand);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return;
+            }
 
-
+            System.out.println(createMongoFind(sqlCommand));
         }
 
 
@@ -74,6 +131,7 @@ public class Main {
         Translator translator = new Translator();
         translator.convert("SELECT name, surname, job FROM workers");
         translator.convert("SELECT name FROM collection OFFSET 5 LIMIT 10");
+        translator.convert("SELECT * FROM customers WHERE age > 22 AND name = 'Vasya' AND age <> 35 AND salary < 455 OFFSET 10 LIMIT 50");
 
 
     }
